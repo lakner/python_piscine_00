84bcb0ba524b71a870fc35b976d7634b1a3f369a
lakner <stephanie.lakner@rub.de>
2024-06-21 21:15:03 +0200

    Add zero/NaN checks for all types

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -4,17 +4,17 @@ def NULL_not_found(object: any) -> int:
     name = "Type not Found"
     if object is None:
         name = "Nothing"
-    elif (isinstance(object, float)):
+    elif (isinstance(object, float) and object != object):
         name = "Cheese"
-    elif (isinstance(object, int)):
+    elif (isinstance(object, int) and not object):
         name = "Zero"
     elif (isinstance(object, str) and not object):
         name = "Empty"
-    elif (isinstance(object, bool)):
+    elif (isinstance(object, bool) and not object):
         name = "Fake"
     else:
         print(name)
         return 1
 
-    print(f"{name}: {type(object)}")
-    return 1
+    print(f"{name}: object {type(object)}")
+    return 0
