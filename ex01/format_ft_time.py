a73c0d06aea9e222bcec71aa5c05846469710e5f
lakner <stephanie.lakner@rub.de>
2024-06-21 17:42:16 +0200

    Fix formats for ex01

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,6 +1,6 @@
 import time
 
-seconds_since_unix_epoch = format(time.time(), ',')
-sci_for = format(time.time(), '.2e')
+seconds_since_unix_epoch = format(time.time(), ",.2f")
+sci_for = format(time.time(), ".2e")
 
 print(f"Seconds since January 1, 1970: {seconds_since_unix_epoch}, or {sci_for} in scientific notation.")
